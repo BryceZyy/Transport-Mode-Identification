535c98e4c7144fc33db654868126d1cb97d8ba2e
BryceZyy <139679507+BryceZyy@users.noreply.github.com>
2024-03-04 18:04:55 +0800

    Create FeatureComputationJob.java

---
+++ data processing/FeatureComputationJob.java
@@ -0,0 +1,33 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class FeatureComputationDriver {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: FeatureComputationDriver <input path> <output path>");
+            System.exit(-1);
+        }
+
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Mobile Data Feature Computation");
+
+        job.setJarByClass(FeatureComputationDriver.class);
+        job.setMapperClass(FeatureMapper.class);
+        job.setReducerClass(FeatureReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        boolean jobCompletedSuccessfully = job.waitForCompletion(true);
+        System.exit(jobCompletedSuccessfully ? 0 : 1);
+    }
+}
