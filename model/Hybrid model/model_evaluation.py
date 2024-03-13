76fe55cf153358a9b5b16d87ddb042ac4d8bec77
BryceZyy <139679507+BryceZyy@users.noreply.github.com>
2024-03-13 16:58:01 +0800

    Update model_evaluation.py

--- model/Hybrid model/model_evaluation.py
+++ model/Hybrid model/model_evaluation.py
@@ -3,13 +3,29 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 
+def plot_learning_curve(history):
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
 def plot_confusion_matrix(y_true, y_pred, classes):
     """
     Plot the confusion matrix.
     """
     cm = confusion_matrix(y_true, y_pred)
     cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-    
+
     plt.figure(figsize=(12, 12))
     sns.heatmap(cm_normalized, annot=True, cmap='Blues', xticklabels=classes, yticklabels=classes)
     plt.show()
