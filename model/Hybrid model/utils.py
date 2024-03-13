8b5a0767fe274199d9b4c877707a27ec945b314b
BryceZyy <139679507+BryceZyy@users.noreply.github.com>
2024-03-13 16:58:30 +0800

    Update utils.py

--- model/Hybrid model/utils.py
+++ model/Hybrid model/utils.py
@@ -12,39 +12,38 @@ def create_dataset(X, look_back):
     Returns:
     - dataX: numpy array containing sequences of data.
     """
-    length = X.shape[1] - 1  # Exclude the identifier column from the feature set
-    dataX = []
-    i = 0
+
+    lenth = X.shape[1] - 1
+    mat = X[0, 1:]
+    fillmat = np.full([look_back - 1, lenth], 0)
+    dataX = np.vstack((fillmat, mat))
+    premdn = X[0, 0]
+    i = 1
+    count = 1
     while i < len(X):
-        if i + look_back <= len(X):
-            # Check if the sequence breaks within the look_back period due to a change in identifier
-            if len(set(X[i:i+look_back, 0])) == 1:
-                sequence = X[i:i+look_back, 1:]  # Exclude identifier from features
-                dataX.append(sequence)
+        if (X[i][0] == premdn):
+            if (count < look_back):
+                temp = X[i - count:i + 1, 1:]
+                filltemp = fill_zeros(temp, look_back, lenth)
+                dataX = np.vstack((dataX, filltemp))
             else:
-                # Handle sequences crossing identifier boundaries
-                sequence = fill_zeros(X[i, 1:].reshape(1, -1), look_back, length)
-                dataX.append(sequence)
-        i += 1
+                dataX = np.vstack((dataX, X[i - look_back + 1:i + 1, 1:]))
+            i += 1
+            count += 1
+        else:
+            mat = X[i, 1:]
+            fillmat = np.full([look_back - 1, lenth], 0)
+            fillmat = np.vstack((fillmat, mat))
+            dataX = np.vstack((dataX, fillmat))
+            premdn = X[i, 0]
+            count = 1
+            i += 1
     return np.array(dataX)
 
-def fill_zeros(mat, look_back, length):
-    """
-    Pad the input matrix with zeros on top to ensure it has the desired number of rows (look_back).
-    This is used for handling sequences shorter than the look_back period.
-
-    Parameters:
-    - mat: numpy array, the matrix to be padded.
-    - look_back: int, desired number of rows in the output matrix.
-    - length: int, number of features in the input matrix.
-
-    Returns:
-    - numpy array, the padded matrix with dimensions (look_back, length).
-    """
+def fill_zeros(mat, look_back, lenth):
     if mat.shape[0] < look_back:
-        zero_pad = np.zeros((look_back - mat.shape[0], length))
-        padded_mat = np.vstack((zero_pad, mat))
-        return padded_mat
+        temp = np.full([look_back - mat.shape[0], lenth], 0)
+        temp = np.vstack((temp, mat))
+        return temp
     else:
         return mat
-
